00380e12d91f760e12f5e8e3c47a2a7687317c41
Nick Budak <budak@stanford.edu>
2025-07-23 15:08:37 -0700

    Select the dist output target as primary
    This is the best way for us to publish output, because we want to
    offer the viewer as a single autoloaded monolithic component.
    
    Closes #3

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,34 +2,18 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'ogm-viewer',
+  validatePrimaryPackageOutputTarget: true,
   outputTargets: [
+    // This is the build target used by apps that will consume the viewer.
     {
       type: 'dist',
       esmLoaderPath: '../loader',
-    },
-    {
-      type: 'dist-custom-elements',
-      customElementsExportBehavior: 'auto-define-custom-elements',
-      externalRuntime: false,
-      copy: [
-        {
-          src: '../node_modules/@shoelace-style/shoelace/dist/assets/icons',
-          dest: 'dist/assets/icons',
-          warn: true,
-        },
-      ],
+      isPrimaryPackageOutputTarget: true,
     },
     // This target is used only for the development live server.
     {
       type: 'www',
       serviceWorker: null, // disable service workers
-      copy: [
-        {
-          src: '../node_modules/@shoelace-style/shoelace/dist/assets/icons',
-          dest: 'assets/icons',
-          warn: true,
-        },
-      ],
     },
   ],
   testing: {
