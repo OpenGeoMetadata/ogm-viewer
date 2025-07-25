9f4cd7a14d6b8fee125bfb6c3846c039c0643d5d
Nick Budak <budak@stanford.edu>
2025-07-25 13:47:00 -0700

    Remove unused tooltip

--- src/components/ogm-menubar/ogm-menubar.tsx
+++ src/components/ogm-menubar/ogm-menubar.tsx
@@ -2,7 +2,6 @@ import { Component, Prop, EventEmitter, h, Event } from '@stencil/core';
 import type { OgmRecord } from '../../utils/record';
 
 import _SlIconButton from '@shoelace-style/shoelace/dist/components/icon-button/icon-button.component.js';
-import _SlTooltip from '@shoelace-style/shoelace/dist/components/tooltip/tooltip.component.js';
 
 @Component({
   tag: 'ogm-menubar',
@@ -18,9 +17,7 @@ export class OgmMenubar {
     if (!this.record) return;
     return (
       <div class="menubar">
-        <sl-tooltip content="Open sidebar">
-          <sl-icon-button name="list" label="Open sidebar" class="menu-button" onclick={this.sidebarToggled.emit}></sl-icon-button>
-        </sl-tooltip>
+        <sl-icon-button name="list" label="Open sidebar" class="menu-button" onclick={this.sidebarToggled.emit}></sl-icon-button>
         <div class="title">{this.record.title}</div>
       </div>
     );
